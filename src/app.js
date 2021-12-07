b2f3b494503e5e9e04deb39dafb99c7eb2f31eae
Priyanka-Prajapatii <priyanka.prajapaatii@gmail.com>
2021-12-07 16:08:03 +0530

    adding back button

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.get('/weather',(req,res)=>{
 
 app.get('*',(req,res)=>{
     res.render(`404error`,{
-        errorMsg: `Opps! Page Not Found`
+        errorMsg: `Opps! Page Not Found, Click hereto go back!`
     });
 })
 
